d82b69a893b082a7b6aaf7501a894a89bed2c6df
Aleksipa <paavola.aleksi@gmail.com>
2020-03-23 18:38:43 +0200

    Loput testit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,46 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void kortilleVoiLadataRahaa() {
+    kortti.lataaRahaa(2500);
+        assertEquals("saldo: 35.0", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeJosRahaaOnTarpeeksi() {
+    kortti.otaRahaa(100);
+        assertEquals("saldo: 9.0", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+    kortti.otaRahaa(10000);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void metodiPalauttaaTrueJosRahatRiittivat() {
+    //kortti.otaRahaa(100);
+        assertTrue(kortti.otaRahaa(100));
+        assertFalse(kortti.otaRahaa(10000));
+    }
+    @Test
+    public void saldoMetodiPalauttaaOikeanSumman() {
+    kortti.otaRahaa(100);
+    assertEquals(900, kortti.saldo());
     }
 }
