24a293cac5807c152c0a0934d43ab3155c8618c2
Aleksipa <paavola.aleksi@gmail.com>
2020-04-06 12:27:39 +0300

    project init

--- pomodoroApp/src/main/java/pomodoroapp/Main.java
+++ pomodoroApp/src/main/java/pomodoroapp/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.pomodoroapp;
+package pomodoroapp;
 
 /**
  *
  * @author aleksipaavola
  */
-public class Paaohjelma {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
