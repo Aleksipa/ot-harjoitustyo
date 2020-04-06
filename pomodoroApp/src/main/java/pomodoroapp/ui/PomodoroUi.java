6dc4d5801e3aea190073b77536d4e7bc7e6fdc51
Aleksipa <paavola.aleksi@gmail.com>
2020-04-06 19:38:26 +0300

    gui added

--- pomodoroApp/src/main/java/pomodoroapp/ui/PomodoroUi.java
+++ pomodoroApp/src/main/java/pomodoroapp/ui/PomodoroUi.java
@@ -7,16 +7,35 @@ package pomodoroapp.ui;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
  *
  * @author aleksipaavola
  */
-public class PomodoroUi {
+public class PomodoroUi extends Application {
     
+    @Override
     public void start(Stage ikkuna) {
-        ikkuna.setTitle("Hei Maailma!");
+        
+        Button lisaaNappi = new Button("Aloita");
+         
+        GridPane komponenttiryhma = new GridPane();
+         
+        komponenttiryhma.add(lisaaNappi, 1, 2);
+        
+        komponenttiryhma.setHgap(10);
+        komponenttiryhma.setVgap(10);
+        komponenttiryhma.setPadding(new Insets(40, 100, 40, 100));
+        
+        Scene nakyma = new Scene(komponenttiryhma);
+        
+        ikkuna.setScene(nakyma);
+        ikkuna.setTitle("Pomodoro App");
         ikkuna.show();
     }
     
